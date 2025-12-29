eee33b5fb4f48267981a7e3a1427a34728fbdcaa
LemuelAbellana <labellana_230000002074@uic.edu.ph>
2025-12-29 19:57:29 +0800

    Add initial Laravel application bootstrap files
    Added core Laravel bootstrap files including artisan, bootstrap/app.php, cached package and service provider files, and initial route and log files. Also updated package.json and added lock files for Composer and npm dependencies to set up the project environment.

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,41 @@
+<?php return array (
+  'livewire/livewire' => 
+  array (
+    'aliases' => 
+    array (
+      'Livewire' => 'Livewire\\Livewire',
+    ),
+    'providers' => 
+    array (
+      0 => 'Livewire\\LivewireServiceProvider',
+    ),
+  ),
+  'nesbot/carbon' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Carbon\\Laravel\\ServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'nunomaduro/termwind' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Termwind\\Laravel\\TermwindServiceProvider',
+    ),
+  ),
+  'spatie/laravel-honeypot' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Spatie\\Honeypot\\HoneypotServiceProvider',
+    ),
+  ),
+);
